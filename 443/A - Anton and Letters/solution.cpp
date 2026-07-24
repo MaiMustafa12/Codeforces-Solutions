aa41b251faa6242caf6e8f576379996f6ecd2a5e
Mai Mustafa <myymhhhyu@gmail.com>
2026-07-24 16:47:18 +0300

    Add Anton and Letters [A] from Codeforces

---
+++ 443/A - Anton and Letters/solution.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<string>
+#include<set>
+using namespace std;
+int main(){
+string s;
+getline(cin,s);
+set<char> d;
+for(char c:s){
+if(c>='a'&&c<='z')d.insert(c);
+}
+cout<<d.size()<<"
+";
+return 0;
+}
